aa98c83a2e0581ccda3cb2dec0f8c55e0bf368f1
jim9112 <jim9112@gmail.com>
2021-01-20 10:10:59 -0500

    style search box

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,17 +1,72 @@
-import { AppBar, IconButton, Toolbar } from '@material-ui/core';
+import {
+  AppBar,
+  IconButton,
+  InputBase,
+  Link,
+  Toolbar,
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles } from '@material-ui/core/styles';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade, makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 const TopNav = () => {
+  const classes = useStyles();
   return (
     <div>
-      <AppBar position="static">
+      <AppBar position="static" color="transparent" variant="outlined">
         <Toolbar>
           <IconButton edge="start" aria-label="menu">
             <MenuIcon />
           </IconButton>
+          <Link>Jmail</Link>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search mail"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
         </Toolbar>
       </AppBar>
     </div>
